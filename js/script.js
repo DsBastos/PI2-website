7f621dd2bbac7b312b4702becd738d428537696c
Daniel Bastos <estgv18113@alunos.estgv.ipv.pt>
2022-01-25 18:32:14 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-function scrollUp(){
-  const scrollUp = document.getElementById('scroll-up');
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
   // Quando o 'scroll' é maior que 560 de altura da 'viewport', adicionar a classe 'show-scroll' para a tag com 'scrollUp'
-  if(this.scrollY >= 100) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+  if (this.scrollY >= 100) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp);
+window.addEventListener("scroll", scrollUp);
